e073797c98c97c0c296fef765e90de7c62f150b6
grainiek <andersen_tobias@hotmail.com>
2016-10-04 19:51:57 +0200

    Fixed light detection system

--- Assets/Scripts/LightAmountSystem/LightColisionDetection.cs
+++ Assets/Scripts/LightAmountSystem/LightColisionDetection.cs
@@ -3,44 +3,58 @@ using System.Collections;
 
 public class LightColisionDetection : MonoBehaviour {
 
-    public float colRange = 0; 
+    public float colRange = 0;
+    public float colSunHit;
     public Light[] lightArray;
-    public int n = 0; 
+    public GameObject sunLight;
+    public int n = 0;
+    public LayerMask layerMask;
+    public GameObject amountVisualizer; 
+
 
     // Use this for initialization
     void Start () {
 	    lightArray = new Light[20];
+        sunLight = GameObject.Find("Directional Light");
     }
 
     void OnTriggerEnter(Collider col)
     {
         lightArray[n] = col.GetComponent<Light>();
         n++; 
-        if (n == 20)
-        {
-            n = 0;
-        }
+        
     }
 
     void OnTriggerStay(Collider col)
     {
-        
+
         //colLight.intensity = colLight.range - dist;
 
-        colRange = 0;
+        colRange = 0; 
+        
         for (int i = 0; i < 20; i++)
         {
             if (lightArray[i] != null)
             {
                 float dist = Vector3.Distance(lightArray[i].transform.position, transform.position);
                 Light colLight = lightArray[i].GetComponent<Light>();
-                colRange = colRange + ((colLight.range - dist)/colLight.range);
-            }
-             
-        }
 
+                if (Physics.Raycast(transform.position, (lightArray[i].transform.position - transform.position), dist, layerMask) == true && colLight.shadows != LightShadows.None)
+                {
+                    colRange = colRange + 0; 
+                }
+                else
+                {
+                    if ((((colLight.range - dist) / colLight.range)) > 0)
+                    {
+                        colRange = colRange + (Mathf.Pow(((colLight.range - dist) / colLight.range), 2.3f) * (colLight.intensity));
+                        
+                    }
+                }
+            }
+        }
         //colRange = colLight.range - dist;
-        gameObject.GetComponent<Renderer> ().material.color = new Vector4(colRange,0,0,0);
+        amountVisualizer.GetComponent<Renderer>().material.color = new Vector4(colRange, 0, 0, 0);
     }
 
     void OnTriggerExit(Collider col)
@@ -53,11 +67,31 @@ public class LightColisionDetection : MonoBehaviour {
             }
 
         }
-
     }
 
     // Update is called once per frame
     void Update () {
-	
-	}
+        colSunHit = 0;
+        if (n == 20)
+        {
+            n = 0;
+        }
+
+        Quaternion sunQuat = sunLight.transform.rotation;
+        Vector3 sunDir = sunQuat * Vector3.forward;
+
+        if (Physics.Raycast(transform.position, (-sunDir), 100, layerMask) == true)
+        {
+            colSunHit = colSunHit + 0;
+        }
+        else
+        {
+            Light sunShadowCheck = sunLight.GetComponent<Light>();
+            colSunHit = colSunHit + (sunShadowCheck.intensity);
+        }
+
+        if (colRange < 0) { colRange = 0; }
+        if (colSunHit < 0) { colSunHit = 0; }
+        amountVisualizer.GetComponent<Renderer>().material.color = new Vector4(colRange + colSunHit, 0, 0, 0);
+    }
 }
---
+++ Assets/Scripts/LightAmountSystem/PlayerDetectorLight.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDetectorLight : MonoBehaviour {
+
+    public GameObject player;
+    public float lightTolerance;
+    public float playerDistanceTolerance;
+    public float dectectionTimeTolerance;
+    public LayerMask layerMask;
+    public float pleyerDetected = 0;
+    public float distanceXLight = 0.5f;
+
+    private float detectTimer;
+    private bool playerOutOfRange = true;
+    public float playerLumDist; 
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+
+        float colRange = player.GetComponent<LightColisionDetection>().colRange;
+        float colSunHit = player.GetComponent<LightColisionDetection>().colSunHit;
+        float playerLightIntensity = colRange + colSunHit;
+        float seePlayer = 0; 
+
+        
+        if (Physics.Raycast(transform.position, (player.transform.position - transform.position), dist, layerMask) == false)
+        {
+            //Debug.DrawRay(transform.position, (player.transform.position - transform.position), Color.green);
+
+            if (playerDistanceTolerance < dist) { playerOutOfRange = true; }
+            else { playerOutOfRange = false; }
+
+            if (dist > 0)
+            {
+                playerLumDist = playerLightIntensity / (dist * distanceXLight);
+            }
+
+
+            if (playerLumDist > lightTolerance && playerOutOfRange == false)
+            {
+                //gameObject.GetComponent<Renderer>().material.color = new Vector4(0, 0, 1, 0);
+                pleyerDetected = pleyerDetected + dectectionTimeTolerance;
+                if (pleyerDetected > 100) { pleyerDetected = 100; }
+                seePlayer = 1;
+            }
+            else
+            {
+                //gameObject.GetComponent<Renderer>().material.color = new Vector4(0, 0, 0, 0);
+                pleyerDetected = pleyerDetected - (dectectionTimeTolerance * 0.25f);
+                if (pleyerDetected < 0) { pleyerDetected = 0; }
+                seePlayer = 0;
+            }
+        }
+        else
+        {
+            pleyerDetected = pleyerDetected - (dectectionTimeTolerance*0.25f);
+            if (pleyerDetected < 0) { pleyerDetected = 0; }
+        }
+        if (pleyerDetected >= 10)
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Vector4(1, (pleyerDetected*0.02f), 0, seePlayer);
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Vector4(0.3f, 0.3f, seePlayer, 0);
+        }
+    }
+}
